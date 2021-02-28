669d2a0a607353cd5c65213be46aff3726d7f8c6
Niko Pettersen <niko.pettersen@newthings.co>
2021-02-28 23:36:20 +0200

    Add i18n to rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,15 @@ import typescript from 'rollup-plugin-typescript2';
 
 export default [
   {
-    external: ['chalk', 'consola', 'discord.js', 'dotenv'],
+    external: [
+      'chalk',
+      'consola',
+      'discord.js',
+      'dotenv',
+      'i18next',
+      'i18next-fs-backend',
+      'path'
+    ],
     input: ['src/index.ts'],
     output: [
       { dir: 'dist', format: 'esm', entryFileNames: '[name].mjs', sourcemap: true }
