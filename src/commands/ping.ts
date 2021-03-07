15f5380ee6bae91ac882cc371eef9f120ce02c2a
Niko Pettersen <niko.pettersen@newthings.co>
2021-03-07 11:00:46 +0200

    Add current language to ping response

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -3,9 +3,10 @@ import { i18n } from '../utils/i18n';
 import { logSuccess } from '../utils/logger';
 
 async function commandPing(message: Discord.Message): Promise<void> {
+  const { language } = i18n;
   const timeTaken = message.createdTimestamp - Date.now();
   const version = process.env.npm_package_version;
-  const replyMessage = i18n.t('responses:ping', { timeTaken, version });
+  const replyMessage = i18n.t('responses:ping', { language, timeTaken, version });
   const response = await message.reply(replyMessage);
   return logSuccess({ message, replyMessage, response });
 }
